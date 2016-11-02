1c906c584298e0063140dfdfa539d84b44208cba
Klaus Gaarde <klausgaarde@live.dk>
2016-11-02 15:16:27 +0100

    Arbejder på tabel

---
+++ WebUI/Scripts/Custom.js
@@ -0,0 +1,30 @@
+﻿google.charts.load('current', { 'packages': ['bar'] });
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Måned', 'Emails pr. Måned'],
+      ['Januar', 47],
+      ['Februar', 86],
+      ['Marts', 32],
+      ['April', 90],
+      ['Maj', 99],
+      ['Juni', 45],
+      ['Juli', 54],
+      ['August', 66],
+      ['September', 55],
+      ['Oktober', 100],
+      ['November', 40],
+      ['December', 76]
+    ]);
+
+    var options = {
+        chart: {
+            title: 'Email aktivitet'
+        }
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('columnchart_material2z'));
+
+    chart.draw(data, options);
+
+}
